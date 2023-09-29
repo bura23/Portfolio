45bd95305eac71434560b3dd9f4647c209932196
bura23 <habtamubiruk23@gmail.com>
2023-09-29 16:37:15 +0300

    Initial Commit

---
+++ src/pages/projects/Movie_Streaming/Slice.js
@@ -0,0 +1,177 @@
+import sliceAnnotationLarge from 'assets/slice-annotation-large.png';
+import sliceAnnotationPlaceholder from 'assets/slice-annotation-placeholder.png';
+import sliceAnnotation from 'assets/slice-annotation.png';
+import sliceAppLarge from 'assets/slice-app-large.jpg';
+import sliceAppPlaceholder from 'assets/slice-app-placeholder.jpg';
+import sliceApp from 'assets/slice-app.jpg';
+import sliceBackgroundBarLarge from 'assets/slice-background-bar-large.jpg';
+import sliceBackgroundBarPlaceholder from 'assets/slice-background-bar-placeholder.jpg';
+import sliceBackgroundBar from 'assets/slice-background-bar.jpg';
+import sliceBackgroundLarge from 'assets/slice-background-large.jpg';
+import sliceBackgroundPlaceholder from 'assets/slice-background-placeholder.jpg';
+import sliceBackground from 'assets/slice-background.jpg';
+import sliceIrlPlaceholder from 'assets/slice-irl-placeholder.jpg';
+import sliceIrl from 'assets/slice-irl.jpg';
+import sliceSidebarAnnotationsLarge from 'assets/slice-sidebar-annotations-large.png';
+import sliceSidebarAnnotationsPlaceholder from 'assets/slice-sidebar-annotations-placeholder.png';
+import sliceSidebarAnnotations from 'assets/slice-sidebar-annotations.png';
+import sliceSidebarLayersLarge from 'assets/slice-sidebar-layers-large.png';
+import sliceSidebarLayersPlaceholder from 'assets/slice-sidebar-layers-placeholder.png';
+import sliceSidebarLayers from 'assets/slice-sidebar-layers.png';
+import sliceSlidesLarge from 'assets/slice-slides-large.jpg';
+import sliceSlidesPlaceholder from 'assets/slice-slides-placeholder.jpg';
+import sliceSlides from 'assets/slice-slides.jpg';
+import { Footer } from 'components/Footer';
+import { Image } from 'components/Image';
+import { Meta } from 'components/Meta';
+import {
+  ProjectBackground,
+  ProjectContainer,
+  ProjectHeader,
+  ProjectImage,
+  ProjectSection,
+  ProjectSectionColumns,
+  ProjectSectionContent,
+  ProjectSectionHeading,
+  ProjectSectionText,
+  ProjectTextRow,
+} from 'layouts/Project';
+import { Fragment } from 'react';
+import { media } from 'utils/style';
+import styles from './Slice.module.css';
+
+const title = 'Korean Movie Streaming Platform';
+const description =
+  'This project involved designing a better experiance for korean users talaiored with thier stayle.';
+const roles = ['User Research', 'UX Design', 'Interface Design'];
+
+export const Slice = () => {
+  return (
+    <Fragment>
+      <Meta title={title} prefix="Projects" description={description} />
+      <ProjectContainer className={styles.slice}>
+        <ProjectBackground
+          src={sliceBackground}
+          srcSet={`${sliceBackground.src} 1280w, ${sliceBackgroundLarge.src} 2560w`}
+          placeholder={sliceBackgroundPlaceholder}
+          opacity={0.8}
+        />
+        <ProjectHeader
+          title={title}
+          description={description}
+          // url="https://www.best.edu.au/s/q2yjjvl7?data=8%404!9%4020303!10%40-15087&version=1"
+          roles={roles}
+        />
+        <ProjectSection padding="top">
+          <ProjectSectionContent>
+            <ProjectImage
+              srcSet={[sliceApp, sliceAppLarge]}
+              placeholder={sliceAppPlaceholder}
+              alt="The korean Movie streaming platform web application showing a selected user annotation."
+              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 90vw, 80vw`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionColumns centered className={styles.columns}>
+            <div className={styles.imagesText}>
+              <ProjectSectionHeading>Bringing it together</ProjectSectionHeading>
+              <ProjectSectionText>
+                Korean users sought an enhanced cinematic experience infused with authentic Korean aesthetics and 
+                cultural elements when viewing movies.
+              </ProjectSectionText>
+              <ProjectSectionText>
+              Our solution entailed enabling users to engage with meticulously curated content 
+              that is uniquely tailored to meet their specific preferences and requirements.
+              </ProjectSectionText>
+            </div>
+            <div className={styles.sidebarImages}>
+              <Image
+                className={styles.sidebarImage}
+                srcSet={[sliceSidebarLayers, sliceSidebarLayersLarge]}
+                placeholder={sliceSidebarLayersPlaceholder}
+                alt="The layers sidebar design, now Simple Informations."
+                sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
+              />
+              <Image
+                className={styles.sidebarImage}
+                srcSet={[sliceSidebarAnnotations, sliceSidebarAnnotationsLarge]}
+                placeholder={sliceSidebarAnnotationsPlaceholder}
+                alt="Multiple functions tailored with users need."
+                sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
+              />
+            </div>
+          </ProjectSectionColumns>
+        </ProjectSection>
+        <ProjectSection light>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Improving the experience</ProjectSectionHeading>
+              <ProjectSectionText>
+              Increasingly, Korean cinema enthusiasts are encountering a challenge when seeking out their favorite 
+              films on existing streaming platforms, as these services often lack comprehensive access to a diverse range 
+              of Korean movies, thereby limiting the depth and breadth of their cinematic experiences
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              srcSet={[sliceSlides, sliceSlidesLarge]}
+              placeholder={sliceSlidesPlaceholder}
+              alt="The new korean movie streaming platform."
+              sizes={`(max-width: ${media.mobile}px) 500px, (max-width: ${media.tablet}px) 800px, 1000px`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection padding="top">
+          <ProjectSectionContent className={styles.grid}>
+            <div className={styles.gridImage}>
+              <div className={styles.gridBackground}>
+                <Image
+                  srcSet={[sliceBackgroundBar, sliceBackgroundBarLarge]}
+                  placeholder={sliceBackgroundBarPlaceholder}
+                  alt=""
+                  role="presentation"
+                  sizes={`(max-width: ${media.mobile}px) 312px, (max-width: ${media.tablet}px) 408px, 514px`}
+                />
+              </div>
+              <div className={styles.gridForeground}>
+                <Image
+                  srcSet={[sliceAnnotation, sliceAnnotationLarge]}
+                  placeholder={sliceAnnotationPlaceholder}
+                  alt="An annotation preview popover with statistics for shape perimeter and area."
+                  sizes={`(max-width: ${media.mobile}px) 584px, (max-width: ${media.tablet}px) 747px, 556px`}
+                />
+              </div>
+            </div>
+            <div className={styles.gridText}>
+              <ProjectSectionHeading>Meaningful details</ProjectSectionHeading>
+              <ProjectSectionText>
+              Enhancing the user experience on our Korean movie streaming platform, 
+              we recognize the imperative to implement advanced features such as precise marking and annotating 
+              of specific scenes or areas within high-resolution films. 
+              </ProjectSectionText>
+            </div>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Project outcomes</ProjectSectionHeading>
+              <ProjectSectionText>
+              This improvement aims to provide our 
+              users with a more immersive and interactive cinematic experience, 
+              where they can engage deeply with the content, gaining insights and sharing their perspectives 
+              in a seamless and sophisticated manner.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              src={sliceIrl}
+              placeholder={sliceIrlPlaceholder}
+              alt="The new korean Movie treaming Platform"
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+      </ProjectContainer>
+      <Footer />
+    </Fragment>
+  );
+};
